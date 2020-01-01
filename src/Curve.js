f81ebd54223f0bf376e53ab5581bcb23f88859e9
Jason Cronquist <jcronq@gmail.com>
2020-01-01 17:33:36 +0000

    update for node

--- src/Curve.js
+++ src/Curve.js
@@ -1,10 +1,9 @@
-function Curve(config)
-{
-	'use strict';
+exports.Curve = function Curve(config){
+    'use strict';
 
-	this.points 		= [];
-	this.currentPoint 	= -1;
-	this.onChange		= config.callback || function(){};
+    this.points 		= [];
+    this.currentPoint 	= -1;
+    this.onChange		= config.callback || function(){};
 
     this.addPoint = function(point){
         if(this.points.length == 0 || point.x > this.points[this.points.length-1].x){
@@ -23,61 +22,61 @@ function Curve(config)
         }
     }
 
-    for (var i=0; i < config.points.length; i++ )
-        this.addPoint(config.points[i]);
-}
+    // Compare 2 points
+    this.isEqual = function(p1,p2)
+    {
+        'use strict';
 
-Curve.prototype.removePoint = function(pointIndex){
-    this.points.splice(pointIndex, 1);
-}
+        if (p1.x == p2.x && p1.y == p2.y) {
+            return true;
+        } else {
+            return false;
+        }
 
-Curve.prototype.movePoint = function(pointIndex, newValue){
-    this.points[pointIndex] = newValue;
-}
-
-// Compare 2 points
-Curve.prototype.isEqual = function(p1,p2)
-{
-	'use strict';
-
-	if (p1.x == p2.x && p1.y == p2.y) {
-		return true;
-	} else {
-		return false;
-	}
-
-}
-
-Curve.prototype.getClosestPointToCoordinate = function(coordinate, threshold=0.1){
-    var pointerX = coordinate.x;
-    var pointerY = coordinate.y;
-
-	var dis = 10000;
-	var clickedPoint = -1;
-
-	for (var i=0;i<this.points.length;i++)
-	{
-		var x1 = pointerX-this.points[i].x;
-		var y1 = pointerY-this.points[i].y;
-
-		var tdis = Math.sqrt(x1*x1+y1*y1);
-		if (tdis < dis && tdis < threshold) { 
-			dis = tdis;
-			clickedPoint = i;
-		}
-	}	
-    return clickedPoint;
-}
-
-// Return the normalized Y value for the specified X value. X should be passed normnalized too
-Curve.prototype.getValueAt = function(xpos) {
-	'use strict';
-
-    var ret = 0;
-    if(this.points.length == 1){
-        ret = this.points[0].y;
+    }
+    this.removePoint = function(pointIndex){
+        this.points.splice(pointIndex, 1);
     }
 
-	return ret;
-}
+    this.movePoint = function(pointIndex, newValue){
+        this.points[pointIndex] = newValue;
+    }
 
+
+    this.getClosestPointToCoordinate = function(coordinate, threshold=0.1){
+        var pointerX = coordinate.x;
+        var pointerY = coordinate.y;
+
+        var dis = 10000;
+        var clickedPoint = -1;
+
+        for (var i=0;i<this.points.length;i++)
+        {
+            var x1 = pointerX-this.points[i].x;
+            var y1 = pointerY-this.points[i].y;
+
+            var tdis = Math.sqrt(x1*x1+y1*y1);
+            if (tdis < dis && tdis < threshold) { 
+                dis = tdis;
+                clickedPoint = i;
+            }
+        }	
+        return clickedPoint;
+    }
+
+    // Return the normalized Y value for the specified X value. X should be passed normnalized too
+    this.getValueAt = function(xpos) {
+        'use strict';
+
+        var ret = 0;
+        if(this.points.length == 1){
+            ret = this.points[0].y;
+        }
+
+        return ret;
+    }
+    if (config.points){
+        for (var i=0; i < config.points.length; i++ )
+            this.addPoint(config.points[i]);
+    }
+}
